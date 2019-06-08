17d1a340c6dbc928060f1bcffa7141870a25c467
Ramiz Raja <informramiz@gmail.com>
2019-06-08 14:07:46 +0500

    feat: Practice param by reference

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <math.h>
+
+void basicPointerAssignment() {
+    int val = 20;
+    int* ptr = nullptr;
+    ptr = &val;
+    
+    std::cout << "value of val: " << val << std::endl;
+    std::cout << "&val: " << &val << std::endl;
+    std::cout << "ptr: " << ptr << std::endl;
+    std::cout << "*ptr: " << *ptr << std::endl;
+}
 
 void initializeBasicPointer() {
     //create int memory on heap
@@ -12,16 +24,24 @@ void initializeBasicPointer() {
     delete intPtr;
 }
 
+void trigonometry(double angle, double* _sin, double* _cos) {
+    *_sin = sin(angle);
+    *_cos = cos(angle);
+}
+
+void tryParamsByReference() {
+    double angle, _sin, _cos;
+    std::cout << "Enter angel: ";
+    std::cin >> angle;
+    
+    //calculate sin cose
+    trigonometry(angle, &_sin, &_cos);
+    //print sin cos
+    std::cout << "Sin: " << _sin << std::endl;
+    std::cout << "Cos: " << _cos << std::endl;
+}
+
 int main() {
-    int val = 20;
-    int* ptr = nullptr;
-    ptr = &val;
-    
-    std::cout << "value of val: " << val << std::endl;
-    std::cout << "&val: " << &val << std::endl;
-    std::cout << "ptr: " << ptr << std::endl;
-    std::cout << "*ptr: " << *ptr << std::endl;
-    
-    initializeBasicPointer();
+    tryParamsByReference();
     return 0;
 }
